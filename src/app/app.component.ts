6fb87061937d1318ed987bf51811295dc009d1aa
gfriedtod <wilfried.wayou@2026.ucac-icam.com>
2024-06-26 19:59:38 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HlmButtonDirective} from "@spartan-ng/ui-button-helm";
+import {
+  HlmAccordionContentDirective,
+  HlmAccordionDirective, HlmAccordionIconDirective,
+  HlmAccordionImports,
+  HlmAccordionItemDirective,
+  HlmAccordionTriggerDirective
+} from "@spartan-ng/ui-accordion-helm";
+import {HlmIconComponent} from "@spartan-ng/ui-icon-helm";
+import {BrnAccordionContentComponent} from "@spartan-ng/ui-accordion-brain";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HlmButtonDirective, HlmButtonDirective, HlmButtonDirective, HlmButtonDirective, HlmButtonDirective, HlmAccordionDirective, HlmAccordionImports, HlmAccordionDirective, HlmAccordionImports, HlmAccordionItemDirective, HlmAccordionTriggerDirective, HlmIconComponent, HlmAccordionContentDirective, HlmAccordionIconDirective, HlmAccordionImports, BrnAccordionContentComponent, HlmAccordionIconDirective, HlmIconComponent, HlmAccordionImports, HlmAccordionIconDirective, HlmIconComponent, HlmAccordionImports, HlmAccordionItemDirective, HlmAccordionImports, HlmAccordionImports, HlmIconComponent, HlmAccordionDirective, HlmAccordionImports, HlmAccordionImports, HlmAccordionImports, HlmAccordionDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'GuoloCahSite';
+  protected readonly _thirdOpened = signal(false);
+  toggleThird() {
+    this._thirdOpened.set(!this._thirdOpened());
+  }
 }
