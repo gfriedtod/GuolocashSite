fcf89913d8d751c0639d79e6632054d44774a95e
gfriedtod <wilfried.wayou@2026.ucac-icam.com>
2024-06-27 12:32:19 +0100

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
   content: [
     './src/**/*.{html,ts}',
-    './REPLACE_WITH_PATH_TO_YOUR_COMPONENTS_DIRECTORY/**/*.{html,ts}',
+    './src/app/components/spartan/**/*.{html,ts}',
   ],
   theme: {
     extend: {},
